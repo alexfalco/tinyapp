047908bef3ec76ec8275a096eecb1838d3922f11
alexfalco <afalconer02@gmail.com>
2019-12-10 16:33:03 -0500

    yeah

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ app.set("view engine", "ejs");
 
 
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+  console.log(urlDatabase)
 });
 
 
@@ -27,6 +29,16 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+
+
+
 
 
 app.get("/hello", (req, res) => {
