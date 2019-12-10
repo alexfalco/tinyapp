ccfed6cca08073132137ab34573db9c7c594d182
alexfalco <afalconer02@gmail.com>
2019-12-10 17:37:26 -0500

    10 dec 17:37

--- express_server.js
+++ express_server.js
@@ -2,7 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
+
+function generateRandomString() {
+  const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  
+  let string_length = 6;
+  let randomstring = '';
+   
+  for (let i = 0; i < string_length; i++) {
+  let rnum = Math.floor(Math.random() * chars.length);
+  randomstring += chars.substring(rnum, rnum + 1);
+  }
+   
+  return randomstring;
+  
+}
 
 
 
@@ -31,10 +48,28 @@ app.get("/urls", (req, res) => {
 });
 
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
+
+
+
+
 
 
 
